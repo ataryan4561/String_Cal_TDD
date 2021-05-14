785746b198fc82e31added07689597bca0427434
ataryan4561 <ataryan4561@gmail.com>
2021-05-14 14:21:17 +0530

    7th Commit

--- test/test.java
+++ test/test.java
@@ -8,3 +8,13 @@ import static org.junit.Assert.assertEquals;
 public class test {
 
     private StringCalculator calculator;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void initialize() 
+    {
+        calculator = new StringCalculator();
+    }
+    
