79241fda6819d33cf6a90e3e3cc94d2038a0d396
ataryan4561 <ataryan4561@gmail.com>
2021-05-14 14:10:53 +0530

    Second commit

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,4 +2,14 @@ public class StringCalculator {
     public int add(String input) {
         String[] numbers = input.split(",|\n");
 
-        
+        if (input.isEmpty()) 
+        {
+            return 0;
+        }
+         else if (numbers.length > 1) 
+        {
+            return getSum(numbers);
+        }
+        return stringToInt(input);
+    }
+
